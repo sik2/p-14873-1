4602e97729e24397521df87cc0058ec1b2d37f6b
sik2 <bickwang123@gmail.com>
2026-09-17 11:39:45 +0900

    0001

--- src/test/java/com/back/BackApplicationTests.java
+++ src/test/java/com/back/BackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class P148731ApplicationTests {
+class BackApplicationTests {
 
     @Test
     void contextLoads() {

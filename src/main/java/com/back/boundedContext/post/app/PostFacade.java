f110385b1b557ffd3bd5b58657fa01ad175f5e4b
sik2 <bickwang123@gmail.com>
2026-09-17 17:40:26 +0900

    0009

---
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -0,0 +1,28 @@
+package com.back.boundedContext.post.app;
+
+import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.post.domain.Post;
+import com.back.boundedContext.post.out.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostFacade {
+    private final PostRepository postRepository;
+    private final PostWriteUseCase postWriteUseCase;
+
+    public long count() {
+        return postRepository.count();
+    }
+
+    public Optional<Post> findById(int id) {
+        return postRepository.findById(id);
+    }
+
+    public Post write(Member author, String title, String content) {
+        return postWriteUseCase.write(author, title, content);
+    }
+}

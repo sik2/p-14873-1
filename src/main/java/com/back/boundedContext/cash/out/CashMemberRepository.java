232ef0263b1341ad11c4865faa74d51413deef3f
sik2 <bickwang123@gmail.com>
2026-09-18 17:34:26 +0900

    0025

--- src/main/java/com/back/boundedContext/cash/out/CashMemberRepository.java
+++ src/main/java/com/back/boundedContext/cash/out/CashMemberRepository.java
@@ -3,6 +3,8 @@ package com.back.boundedContext.cash.out;
 import com.back.boundedContext.cash.domain.CashMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CashMemberRepository extends JpaRepository<CashMember, Integer> {
-    CashMember findByUsername(String username);
+    Optional<CashMember> findByUsername(String username);
 }

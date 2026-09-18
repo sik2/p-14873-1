05107f03929d472538017c80e7850bccb470212a
sik2 <bickwang123@gmail.com>
2026-09-18 12:43:59 +0900

    0018

--- src/main/java/com/back/boundedContext/member/app/MemberJoinUseCase.java
+++ src/main/java/com/back/boundedContext/member/app/MemberJoinUseCase.java
@@ -2,8 +2,11 @@ package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.member.out.MemberRepository;
+import com.back.global.eventPublisher.EventPublisher;
 import com.back.global.exception.DomainException;
 import com.back.global.rsData.RsData;
+import com.back.shared.member.dto.MemberDto;
+import com.back.shared.member.event.MemberJoinedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberJoinUseCase {
     private final MemberRepository memberRepository;
+    private final EventPublisher eventPublisher;
 
     public RsData<Member> join(String username, String password, String nickname) {
         findByUsername(username).ifPresent(m -> {
@@ -20,6 +24,8 @@ public class MemberJoinUseCase {
         });
 
         Member member = memberRepository.save(new Member(username, password, nickname));
+
+        eventPublisher.publish(new MemberJoinedEvent(new MemberDto(member)));
 
         return new RsData<>("201-1", "%d번 회원이 생성되었습니다.".formatted(member.getId()), member);
     }

71fc54cd7a2451a126288554ceeecc5410c827d3
sik2 <bickwang123@gmail.com>
2026-09-21 10:44:08 +0900

    0028

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -2,8 +2,6 @@ package com.back.boundedContext.post.app;
 
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.domain.PostMember;
-import com.back.boundedContext.post.out.PostMemberRepository;
-import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.rsData.RsData;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -15,18 +13,18 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
-    private final PostRepository postRepository;
     private final PostWriteUseCase postWriteUseCase;
-    private final PostMemberRepository postMemberRepository;
+    private final PostSyncMemberUseCase postSyncMemberUseCase;
+    private final PostSupport postSupport;
 
     @Transactional(readOnly = true)
     public long count() {
-        return postRepository.count();
+        return postSupport.count();
     }
 
     @Transactional(readOnly = true)
     public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+        return postSupport.findById(id);
     }
 
     @Transactional
@@ -36,20 +34,10 @@ public class PostFacade {
 
     @Transactional
     public PostMember syncMember(MemberDto member) {
-        PostMember _member = new PostMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
-        );
-
-        return postMemberRepository.save(_member);
+        return postSyncMemberUseCase.syncMember(member);
     }
 
     public Optional<PostMember> findByUsername(String username) {
-        return postMemberRepository.findByUsername(username);
+        return postSupport.findByUsername(username);
     }
 }

f3193cb6d85a9ad5d07f7abe1869f268cf44207e
sik2 <bickwang123@gmail.com>
2026-09-21 15:04:24 +0900

    0033

--- src/main/java/com/back/boundedContext/market/app/MarketFacade.java
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -1,7 +1,10 @@
 package com.back.boundedContext.market.app;
 
+import com.back.boundedContext.market.domain.Cart;
 import com.back.boundedContext.market.domain.MarketMember;
 import com.back.boundedContext.market.domain.Product;
+import com.back.global.rsData.RsData;
+import com.back.shared.market.dto.MarketMemberDto;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ public class MarketFacade {
     private final MarketSyncMemberUseCase marketSyncMemberUseCase;
     private final MarketSupport marketSupport;
     private final MarketCreateProductUseCase marketCreateProductUseCase;
+    private final MarketCreateCartUseCase marketCreateCartUseCase;
 
     public MarketMember syncMember(MemberDto member) {
         return marketSyncMemberUseCase.syncMember(member);
@@ -51,5 +55,20 @@ public class MarketFacade {
         );
 
         return marketCreateProductUseCase.createProduct(product);
+    }
+
+    @Transactional
+    public RsData<Cart> createCart(MarketMemberDto buyer) {
+        return marketCreateCartUseCase.createCart(buyer);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Cart> findCartByBuyer(MarketMember buyer) {
+        return marketSupport.findCartByBuyer(buyer);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Product> findProductById(int id) {
+        return marketSupport.findProductById(id);
     }
 }
--- src/main/java/com/back/boundedContext/market/app/MarketSyncMemberUseCase.java
+++ src/main/java/com/back/boundedContext/market/app/MarketSyncMemberUseCase.java
@@ -2,6 +2,9 @@ package com.back.boundedContext.market.app;
 
 import com.back.boundedContext.market.domain.MarketMember;
 import com.back.boundedContext.market.out.MarketMemberRepository;
+import com.back.global.eventPublisher.EventPublisher;
+import com.back.shared.market.dto.MarketMemberDto;
+import com.back.shared.market.event.MarketMemberCreatedEvent;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,8 +13,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MarketSyncMemberUseCase {
     private final MarketMemberRepository marketMemberRepository;
+    private final EventPublisher eventPublisher;
 
     public MarketMember syncMember(MemberDto member) {
+        boolean isNew = !marketMemberRepository.existsById(member.getId());
+
         MarketMember _member = new MarketMember(
                 member.getId(),
                 member.getCreateDate(),
@@ -22,6 +28,18 @@ public class MarketSyncMemberUseCase {
                 member.getActivityScore()
         );
 
-        return marketMemberRepository.save(_member);
+        marketMemberRepository.save(_member);
+
+        if (isNew) {
+            eventPublisher.publish(
+                    new MarketMemberCreatedEvent(
+                            new MarketMemberDto(
+                                    _member
+                            )
+                    )
+            );
+        }
+
+        return _member;
     }
 }

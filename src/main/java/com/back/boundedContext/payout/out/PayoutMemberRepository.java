74f597a9748ed5f4da7f3e84e8c49b5907e68ca2
sik2 <bickwang123@gmail.com>
2026-09-22 17:26:28 +0900

    0044

--- src/main/java/com/back/boundedContext/payout/out/PayoutMemberRepository.java
+++ src/main/java/com/back/boundedContext/payout/out/PayoutMemberRepository.java
@@ -4,6 +4,9 @@ import com.back.boundedContext.payout.domain.PayoutMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PayoutMemberRepository extends JpaRepository<PayoutMember, Integer> {
+    Optional<PayoutMember> findByUsername(String username);
 }

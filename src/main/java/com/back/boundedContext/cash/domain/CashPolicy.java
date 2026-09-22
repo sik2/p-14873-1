f5cae1b32e56d94a6d800f45ee4734897ed66d25
sik2 <bickwang123@gmail.com>
2026-09-22 12:07:51 +0900

    0037

--- src/main/java/com/back/boundedContext/cash/domain/CashPolicy.java
+++ src/main/java/com/back/boundedContext/cash/domain/CashPolicy.java
@@ -1,5 +1,12 @@
 package com.back.boundedContext.cash.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class CashPolicy {
-    public static final int HOLDING_MEMBER_ID = 2;
+    public static int HOLDING_MEMBER_ID;
+
+    @Value("${custom.global.holdingMemberId}")
+    public void setHoldingMemberId(int id) {
+        HOLDING_MEMBER_ID = id;
+    }
 }

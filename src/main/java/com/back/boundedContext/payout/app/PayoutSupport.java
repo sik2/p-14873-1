fcf315d11d19fc0870be87f432c657a5c422c333
sik2 <bickwang123@gmail.com>
2026-09-23 10:37:59 +0900

    0045

--- src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
@@ -1,16 +1,20 @@
 package com.back.boundedContext.payout.app;
 
+import com.back.boundedContext.payout.domain.PayoutCandidateItem;
 import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutCandidateItemRepository;
 import com.back.boundedContext.payout.out.PayoutMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class PayoutSupport {
     private final PayoutMemberRepository payoutMemberRepository;
+    private final PayoutCandidateItemRepository payoutCandidateItemRepository;
 
     public Optional<PayoutMember> findSystemMember() {
         return payoutMemberRepository.findByUsername("system");
@@ -20,4 +24,7 @@ public class PayoutSupport {
         return payoutMemberRepository.findById(id);
     }
 
+    public List<PayoutCandidateItem> findPayoutCandidateItems() {
+        return payoutCandidateItemRepository.findAll();
+    }
 }
---
+++ src/main/java/com/back/boundedContext/payout/in/PayoutDataInit.java
@@ -0,0 +1,64 @@
+package com.back.boundedContext.payout.in;
+
+import com.back.boundedContext.payout.app.PayoutFacade;
+import com.back.boundedContext.payout.domain.PayoutPolicy;
+import com.back.shared.Ut.Util;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@Slf4j
+public class PayoutDataInit {
+    private final PayoutDataInit self;
+    private final PayoutFacade payoutFacade;
+    private final JobOperator jobOperator;
+    private final Job payoutCollectItemsJob;
+
+    public PayoutDataInit(
+            @Lazy PayoutDataInit self,
+        PayoutFacade payoutFacade,
+            JobOperator jobOperator,
+            Job payoutCollectItemsJob
+    ) {
+        this.self = self;
+        this.payoutFacade = payoutFacade;
+        this.jobOperator = jobOperator;
+        this.payoutCollectItemsJob = payoutCollectItemsJob;
+    }
+
+    @Bean
+    @Order(4)
+    public ApplicationRunner payoutDataInitApplicationRunner() {
+        return args -> {
+            self.forceMakePayoutReadyCandidatesItems();
+            self.collectPayoutItemsMore();
+        };
+    }
+
+    @Transactional
+    public void forceMakePayoutReadyCandidatesItems() {
+        payoutFacade.findPayoutCandidateItems().forEach(item -> {
+            Util.reflection.setField(
+                    item,
+                    "paymentDate",
+                    LocalDateTime.now().minusDays(PayoutPolicy.PAYOUT_READY_WAITING_DAYS + 1)
+            );
+        });
+    }
+
+    @Transactional
+    public void collectPayoutItemsMore() {
+        payoutFacade.collectPayoutItemsMore(4);
+        payoutFacade.collectPayoutItemsMore(2);
+        payoutFacade.collectPayoutItemsMore(2);
+    }
+}
